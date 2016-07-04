db4aaa6319b76394e666d78395a921b3eb8db111
Artur Dębkowski <arturdebkowski@gmail.com>
2016-07-04 17:42:03 +0200

    adding advanced interaction

--- 2 fikcja interaktywna/dragDrop.js
+++ 2 fikcja interaktywna/dragDrop.js
@@ -1,81 +1,70 @@
-function forEachElements (elemnts, callback) {
-	[].forEach.call(elemnts, callback);
-}
+(function (argument) {
 
-var itemBoxes = document.querySelectorAll('.inventory-box');
-forEachElements(itemBoxes, function(itemBox) {
-	itemBox.addEventListener('dragstart', hadleDragStart);
-	itemBox.addEventListener('dragover', handleDragOver);
-	itemBox.addEventListener('drop', handleDrop);
-});
-
-var draggingObject;
-function handleDragStart (event) {
-	draggingObject = this;
-	event.dataTransfer.setData('text/html', this.innerHTML);
-	var dragIcon = document.createElement('img'),
-		imageName = this.firstChild.id;
-	dragIcon.src = imageName + '.png';
-	event.dataTransfer.setDragImage(dragIcon, -10, 10);
-}
-
-function handleDragOver (event) {
-	event.preventDefault();
-}
-
-function handleDrop (event) {
-	event.preventDefault();
-
-	if (draggingObject == this) {
-		return;
+	function forEachElements (elemnts, callback) {
+		[].forEach.call(elemnts, callback);
 	}
 
-	var draggingGrandpa = draggingObject.parentElement.parentElement,
-		draggedToGrandpa = this.parentElement.parentElement,
-		draggingObjectId = draggingObject.firstChild.id;
-
-	inventoryObject.add(draggedToGrandpa.id, draggingObjectId);
-	inventoryObject.remove(draggingGrandpa.id, draggingObjectId);
-
-	draggingObject.innerHTML = this.innerHTML;
-
-	this.innerHTML = e.dataTransfer.getData('text/html');
-	this.classList.remove('empty');
-
-	draggingObject.classList.add('empty');
-}
-
-var inventoryObject = (function() {
-	var inventory = {},
-		itemables = document.getElementsByClassName("itemable"),
-		items = document.getElementsByClassName("item");
-
-	forEachElements(itemables, function(itemable) {
-		inventory[itemable.id] = [];
+	var itemBoxes = document.querySelectorAll('.inventory-box');
+	forEachElements(itemBoxes, function(itemBox) {
+		itemBox.addEventListener('dragstart', hadleDragStart);
+		itemBox.addEventListener('dragover', handleDragOver);
+		itemBox.addEventListener('drop', handleDrop);
 	});
 
-	forEachElements(items, function(item) {
-		var greatGrandPa = item.parentElement.parentElement.parentElement;
-		inventory[greatGrandPa.id].push(item.id);
-	});
+	var draggingObject;
+	function handleDragStart (event) {
+		draggingObject = this;
+		event.dataTransfer.setData('text/html', this.innerHTML);
+		var dragIcon = document.createElement('img'),
+			imageName = this.firstChild.id;
+		dragIcon.src = imageName + '.png';
+		event.dataTransfer.setDragImage(dragIcon, -10, 10);
+	}
 
-	var add = function (inventorySection, newItem) {
-			inventory[inventorySection].push(newItem);
-			return inventory;
-		},
+	function handleDragOver (event) {
+		event.preventDefault();
+	}
 
-		remove = function (inventorySection, itemToDelete) {
-			for (var i = 0; i < inventory[inventorySection].length; i++) {
-				inventory[inventorySection].splice(i, 1);
-			}
-			return inventory;
-		};
+	function handleDrop (event) {
+		var dragAppliedTo = this;
+		game.things.dropItemInto(draggingObject, dragAppliedTo.parentElement.parentElement.id);
+		event.preventDefault();
+	}
 
-		return {
-			get: function () {
+	var inventoryObject = (function() {
+		var inventory = {},
+			itemables = document.getElementsByClassName("itemable"),
+			items = document.getElementsByClassName("item");
+
+		forEachElements(itemables, function(itemable) {
+			inventory[itemable.id] = [];
+		});
+
+		forEachElements(items, function(item) {
+			var greatGrandPa = item.parentElement.parentElement.parentElement;
+			inventory[greatGrandPa.id].push(item.id);
+		});
+
+		var add = function (inventorySection, newItem) {
+				inventory[inventorySection].push(newItem);
 				return inventory;
 			},
-			add: add,
-			remove: remove
-		};
+
+			remove = function (inventorySection, itemToDelete) {
+				for (var i = 0; i < inventory[inventorySection].length; i++) {
+					inventory[inventorySection].splice(i, 1);
+				}
+				return inventory;
+			};
+
+			return {
+				get: function () {
+					return inventory;
+				},
+				add: add,
+				remove: remove
+			};
+	})();
+
 })();
+
