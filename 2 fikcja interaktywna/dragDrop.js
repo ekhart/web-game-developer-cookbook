b990c302ceb8ed1b643cd424967d216a1b8466d2
Artur Dębkowski <arturdebkowski@gmail.com>
2016-07-04 17:22:23 +0200

    added inventory with drag and drop continued

--- 2 fikcja interaktywna/dragDrop.js
+++ 2 fikcja interaktywna/dragDrop.js
@@ -1,6 +1,9 @@
-var itemBoxes = document.querySelectorAll('.inventory-box');
+function forEachElements (elemnts, callback) {
+	[].forEach.call(elemnts, callback);
+}
 
-[].forEach.call(itemBoxes, function(itemBox) {
+var itemBoxes = document.querySelectorAll('.inventory-box');
+forEachElements(itemBoxes, function(itemBox) {
 	itemBox.addEventListener('dragstart', hadleDragStart);
 	itemBox.addEventListener('dragover', handleDragOver);
 	itemBox.addEventListener('drop', handleDrop);
@@ -42,3 +45,37 @@ function handleDrop (event) {
 	draggingObject.classList.add('empty');
 }
 
+var inventoryObject = (function() {
+	var inventory = {},
+		itemables = document.getElementsByClassName("itemable"),
+		items = document.getElementsByClassName("item");
+
+	forEachElements(itemables, function(itemable) {
+		inventory[itemable.id] = [];
+	});
+
+	forEachElements(items, function(item) {
+		var greatGrandPa = item.parentElement.parentElement.parentElement;
+		inventory[greatGrandPa.id].push(item.id);
+	});
+
+	var add = function (inventorySection, newItem) {
+			inventory[inventorySection].push(newItem);
+			return inventory;
+		},
+
+		remove = function (inventorySection, itemToDelete) {
+			for (var i = 0; i < inventory[inventorySection].length; i++) {
+				inventory[inventorySection].splice(i, 1);
+			}
+			return inventory;
+		};
+
+		return {
+			get: function () {
+				return inventory;
+			},
+			add: add,
+			remove: remove
+		};
+})();
